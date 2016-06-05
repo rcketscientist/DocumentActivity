52e61eba3d26d0f250da1de8ec758e7858127781
Anthony <amandra@gmail.com>
2016-06-05 02:20:01 +0200

    use try-with-resources to cleanup cursor code

--- library/src/main/java/com/anthonymandra/framework/FileUtil.java
+++ library/src/main/java/com/anthonymandra/framework/FileUtil.java
@@ -213,22 +213,17 @@ public class FileUtil
 	public static String getDataColumn(Context context, Uri uri, String selection,
 	                                   String[] selectionArgs) {
 
-		Cursor cursor = null;
 		final String column = "_data";
 		final String[] projection = {
 				column
 		};
 
-		try {
-			cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
-					null);
+		try (Cursor cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, null))
+		{
 			if (cursor != null && cursor.moveToFirst()) {
 				final int column_index = cursor.getColumnIndexOrThrow(column);
 				return cursor.getString(column_index);
 			}
-		} finally {
-			if (cursor != null)
-				cursor.close();
 		}
 		return null;
 	}
@@ -278,21 +273,14 @@ public class FileUtil
 
 	public static String getRealPathFromURI(Context context, Uri contentUri)
 	{
-		Cursor cursor = null;
-		try
+		String[] proj = {MediaStore.Images.Media.DATA};
+		try(Cursor cursor = context.getContentResolver().query(contentUri, proj, null, null, null))
 		{
-			String[] proj = {MediaStore.Images.Media.DATA};
-			cursor = context.getContentResolver().query(contentUri, proj, null, null, null);
+			if (cursor == null)
+				return null;
 			int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
 			cursor.moveToFirst();
 			return cursor.getString(column_index);
-		}
-		finally
-		{
-			if (cursor != null)
-			{
-				cursor.close();
-			}
 		}
 	}
 
