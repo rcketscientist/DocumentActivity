955c8250f196487cd601848a2b5179d2bd6d707d
Anthony Mandra <anthony.mandra@keysight.com>
2018-08-28 14:01:38 -0400

    update to androidx

--- library/src/main/java/com/anthonymandra/framework/DocumentUtil.java
+++ library/src/main/java/com/anthonymandra/framework/DocumentUtil.java
@@ -2,11 +2,12 @@ package com.anthonymandra.framework;
 
 import android.net.Uri;
 import android.provider.DocumentsContract;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.text.TextUtils;
 
 import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 @SuppressWarnings("unused")
 public class DocumentUtil
